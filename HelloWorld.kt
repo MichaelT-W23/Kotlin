546fa2dcfe2de6969c6e4fb16112ac7ccc321357
Michael Totaro <mtotaro@ncsu.edu>
2023-11-11 16:11:01 -0500

    Added HelloWorld file

---
+++ HelloWorld.kt
@@ -0,0 +1,14 @@
+// To run this program type the following commands in your terminal
+
+// cd Kotlin
+// kotlinc HelloWorld.kt -include-runtime -d HelloWorld.jar
+// java -jar HelloWorld.jar
+
+// Note if you want to get rid of the Run | Debug text above main above
+// main add the following line to your settings.json file in VS Code
+
+// "editor.codeLens": false,
+
+fun main() {
+    println("Hello World");
+}
